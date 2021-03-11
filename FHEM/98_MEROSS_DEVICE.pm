57f9aa08f0a2586588455b36faffb058449c90f0
Manolis <mano.pavlakis@gmail.com>
2021-03-11 19:25:58 +0100

    98_MEROSS_DEVICE.pm updated

--- FHEM/98_MEROSS_DEVICE.pm
+++ FHEM/98_MEROSS_DEVICE.pm
@@ -61,8 +61,10 @@ sub MEROSS_DEVICE_Set($@) {
 		}
 	} elsif ($cmd eq "getStatus") {
 	} elsif ($cmd eq "getDeviceType") {
+	} elsif ($cmd eq "reload") {
+		{ system("sudo /opt/fhem/FHEM/meross/meross.py &") }
 	} else {
-		return "Unknown argument $cmd, choose one of open:noArg close:noArg position:slider,0,100,100	 getStatus:noArg getDeviceType:noArg" ;
+		return "Unknown argument $cmd, choose one of open:noArg close:noArg position:slider,0,100,100 getStatus:noArg getDeviceType:noArg reload:noArg" ;
 	}
 }
 
